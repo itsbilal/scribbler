9c99393f383cf66332387ae978d0b8c58f2cac78
Bilal Akhtar <me@itsbilal.com>
2014-10-29 13:46:27 -0400

    Fix line following and implement colour recognition

---
+++ dominantColor.py
@@ -0,0 +1,27 @@
+
+if __name__ == "__main__":
+	from myro import *
+	initialize("/dev/tty.IPRE6-193907-DevB")
+
+
+picture = takePicture("color")
+show(picture)
+blue = 0
+green = 0
+red = 0
+
+for pixel in getPixels(picture):
+	r,g,b = getRGB(pixel)
+	red += r
+	green += g
+	blue += b
+
+
+print "red = %d, green = %d, blue = %d" % (red, green, blue)
+
+if red > green and red > blue:
+	print "Dominant colour: red"
+elif green > red and green > blue:
+	print "Dominant colour: green"
+else:
+	print "Dominant colour: blue"
--- line_following.py
+++ line_following.py
@@ -2,16 +2,21 @@
 from myro import *
 initialize("/dev/tty.IPRE6-193907-DevB")
 
-DEFAULT_SPEED = 0.1
+DEFAULT_SPEED = -0.1
 
 def goStraight():
 	motors(DEFAULT_SPEED, DEFAULT_SPEED)
 
-def turnRightNotch():
-	turnRight(0.25, 0.2)
+def turnRightNotch(counter=1):
+	for i in range(0,counter):
+		turnLeft(0.25, 0.2)
 
-def turnLeftNotch():
-	turnLeft(0.25, 0.2)
+def turnLeftNotch(counter=1):
+	for i in range(0, counter):
+		turnRight(0.25, 0.2)
+
+"""counter = 0
+didGoOffCourse = False"""
 
 while 1:
 	right, left = getLine()
@@ -19,7 +24,21 @@ while 1:
 
 	if left == 1 and right == 0:
 		turnLeftNotch()
+		#counter += 1
 	elif right == 1 and left == 0:
 		turnRightNotch()
+		#counter -= 1
+	"""elif left == 1 and right == 1:
+		if didGoOffCourse:
+			if counter >= 1:
+				turnRightNotch(counter)
+			elif counter <= 1:
+				turnLeftNotch(counter)
+			didGoOffCourse = False
+
+		counter = 0
+	else:
+		didGoOffCourse = True"""
+
 
 	goStraight()
