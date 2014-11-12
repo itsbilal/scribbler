40d920f15f55fbdc3a8aba3bee1e221edaf977fa
Bilal Akhtar <me@itsbilal.com>
2014-11-12 13:24:50 -0500

    Add finalproject.py

---
+++ finalProject.py
@@ -0,0 +1,46 @@
+from myro import *
+from line_following import *
+from time import sleep
+from dominantColor import getColor
+
+from PIL import Image
+from tesserwrap import Tesseract
+
+
+initialize("/dev/tty.IPRE6-193907-DevB")
+
+RUN_OVER_TIME = 2
+OBSTACLE_THRESHOLD = 600
+
+def getPILImage():
+	pic = takePicture("grey")
+	savePicture(pic, "current.png")
+
+	img = Image.open("current.png")
+	return img
+
+def sanitizeInput(inputStr):
+	valid_notes = ["A", "B", "C", "D", "E", "F", "G"]
+
+	inputStr = inputStr.upper()
+
+	for note in valid_notes:
+		if note in inputStr:
+			return note
+
+
+goStraight()
+
+while 1:
+	correctYourself()
+	left, middle,  right = getObstacle()
+	if middle >= OBSTACLE_THRESHOLD:
+		stop()
+		image = getPILImage()
+		ocr = Tesseract().ocr_image(image)
+
+
+
+		sleep(1)
+
+	
